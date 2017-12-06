3f42db26a4a6523c72fefd239091034a840132b3
Michael Kaiser <michael.kaiser@student.uibk.ac.at>
2017-12-06 02:28:41 +0100

    create data for Lorenz curve and Pareto

--- token_distribution/analyze_token_distribution.R
+++ token_distribution/analyze_token_distribution.R
@@ -1,19 +1,17 @@
-# Title     : analyze token distribtion
-# Objective : analyze the distribution of the tokens
-# Created by: michael
-# Created on: 03.12.17
+#merge the files and save merged csv
+
 library(ggplot2)
 library(ineq)
 
-data <- read.csv("../data/qtum.csv")
-print(data)
+symbols <- c("EOS", "ANT", "ATM", "BAT", "BNT")
+data_list <- c()
+for(symbol in symbols){
+  path <- paste("../data/", symbol, sep = "")
+  data <- read.csv("../data/EOS")
+  data <- cbind(data, symbol = rep("EOS", nrow(data)))
+  data_list <- c(data_list, data)
+}
 
-plot <- ggplot(data, aes(x = X, y = tokens)) + geom_line() + scale_y_log10() + scale_x_log10()
+merged_data <- do.call(rbind, data_list)
+print(merged_data)
 
-show(plot)
-
-Lc.p <- Lc(as.vector(data[["tokens"]]))
-plot(Lc.p)
-
-gini_coeff <- ineq(as.vector(data[["tokens"]]), type = "Gini")
-print(gini_coeff)
